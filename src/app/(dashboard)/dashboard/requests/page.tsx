2e319be53d540775940678f2abd10b12867829b9
David Sehi <david.sehi2016@gmail.com>
2023-11-23 19:28:23 +0000

    add right label

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -29,7 +29,7 @@ const page = async () => {
 
   return (
     <main className="pt-8">
-      <h1 className="font-bold text-5xl mb-8">Ajouter un nouvel ami</h1>
+      <h1 className="font-bold text-5xl mb-8">Vos demandes d'ajout</h1>
       <div className="flex flex-col gap-4">
         <FriendRequests
           incomingFriendRequests={incomingFriendRequests}
